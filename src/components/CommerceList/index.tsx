0bc49a60a13fcd713790587c2bf71e45e3ad462b
Pedro Costa <phmc99@outlook.com>
2022-11-20 23:50:55 -0300

    chore: testando componentes chakra ui

---
+++ src/components/CommerceList/index.tsx
@@ -0,0 +1,14 @@
+import { List, ListItem } from '@chakra-ui/react';
+
+const CommerceList = () => {
+  return (
+    <List>
+      <ListItem background="gray.100">Padaria do Anibal</ListItem>
+      <ListItem>Padaria do Anibal</ListItem>
+      <ListItem>Padaria do Anibal</ListItem>
+      <ListItem>Padaria do Anibal</ListItem>
+    </List>
+  );
+};
+
+export default CommerceList;
--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,49 +1,24 @@
 import Router from 'next/router';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../store';
-import { resetAuth } from '../../store/admin';
-import api from '../../services/api';
+import { useEffect } from 'react';
+import { Button, Flex, Heading } from '@chakra-ui/react';
+import CommerceList from '../../components/CommerceList';
 
 const Dashboard = () => {
-  const dispatch = useDispatch<AppDispatch>();
-
-  const [Categories, setCategories] = useState<any[]>([]);
-
-  const getCategories = (tk: string | null) => {
-    api
-      .get('/category', {
-        headers: {
-          Authorization: `Bearer ${tk}`
-        }
-      })
-      .then(res => {
-        setCategories(res.data.categories);
-      })
-      .catch(res => {
-        if (res.request.status === 401) {
-          dispatch(resetAuth());
-          Router.push('/admin/login');
-        }
-      });
-  };
-
   useEffect(() => {
     const token = localStorage.getItem('admin-token');
     token ? Router.push('/admin/dashboard') : Router.push('/admin/login');
-    getCategories(token);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <ul>
-        {Categories.length
-          ? Categories.map((item, index) => <li key={index}>{item.name}</li>)
-          : null}
-      </ul>
-    </div>
+    <Flex height="100vh" justifyContent="center">
+      <Flex direction="column" width={'100%'} p={10}>
+        <Button colorScheme="blue" width={120}>
+          Adicionar
+        </Button>
+        <Heading>Comercios</Heading>
+        <CommerceList />
+      </Flex>
+    </Flex>
   );
 };
 
