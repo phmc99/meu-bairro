2ecec58f8e86f0cbe16ad305290a56bb20d06406
Pedro Costa <phmc99@outlook.com>
2022-11-27 23:53:02 -0300

    feat: adicionando next document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document';
+
+class MyDocument extends NextDocument {
+  static getInitialProps(ctx: DocumentContext) {
+    return NextDocument.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
