882f2c7f0d065b19aac6219cbbc4ef80bcbef0b2
Pedro Costa <phmc99@outlook.com>
2022-11-30 18:44:19 -0300

    feat: consumindo dados do cep no formulario de criacao do comercio

--- src/components/CommerceCreateForm/AddressForm.tsx
+++ src/components/CommerceCreateForm/AddressForm.tsx
@@ -1,6 +1,57 @@
-import { Flex, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react';
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  useToast
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { ChangeEvent, useState } from 'react';
+
+interface CepResponse {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+}
 
 export const AddressForm = () => {
+  const [cep, setCep] = useState<CepResponse>({} as CepResponse);
+  const toast = useToast();
+
+  const getCep = async (cep: string) => {
+    const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+
+    const response = await axios.get(endpoint);
+
+    if (response.data.erro) {
+      toast({
+        title: 'Número ou Formato do CEP inválido.',
+        duration: 3000,
+        isClosable: true,
+        status: 'error',
+        position: 'top'
+      });
+      return;
+    }
+
+    setCep(response.data);
+  };
+
+  const handleChangeCepInput = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length === 8) {
+      getCep(e.target.value);
+    }
+    setCep({} as CepResponse);
+  };
+
   return (
     <>
       <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
@@ -16,6 +67,8 @@ export const AddressForm = () => {
             type="text"
             name="cep"
             id="cep"
+            onChange={handleChangeCepInput}
+            maxLength={8}
           />
         </FormControl>
 
@@ -23,7 +76,13 @@ export const AddressForm = () => {
           <FormLabel htmlFor="neighborhood" fontWeight="md">
             Bairro
           </FormLabel>
-          <Input type="text" name="neighborhood" id="neighborhood" />
+          <Input
+            value={cep.bairro != null ? cep.bairro : ''}
+            disabled
+            type="text"
+            name="neighborhood"
+            id="neighborhood"
+          />
         </FormControl>
       </Flex>
 
@@ -32,14 +91,26 @@ export const AddressForm = () => {
           <FormLabel htmlFor="city" fontWeight="md">
             Cidade
           </FormLabel>
-          <Input type="text" name="city" id="city" />
+          <Input
+            value={cep.localidade != null ? cep.localidade : ''}
+            disabled
+            type="text"
+            name="city"
+            id="city"
+          />
         </FormControl>
 
         <FormControl>
           <FormLabel htmlFor="state" fontWeight="md">
             Estado
           </FormLabel>
-          <Input type="text" name="state" id="state" />
+          <Input
+            value={cep.uf != null ? cep.uf : ''}
+            disabled
+            type="text"
+            name="state"
+            id="state"
+          />
         </FormControl>
       </Flex>
 
@@ -47,7 +118,13 @@ export const AddressForm = () => {
         <FormLabel htmlFor="street" fontWeight="md">
           Rua
         </FormLabel>
-        <Input type="text" name="street" id="street" />
+        <Input
+          value={cep.logradouro != null ? cep.logradouro : ''}
+          disabled
+          type="text"
+          name="street"
+          id="street"
+        />
       </FormControl>
       <Flex gap={2}>
         <FormControl>
