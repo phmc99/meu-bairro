669de43978ffa871ca924e461122bc7b75da1807
Pedro Costa <phmc99@outlook.com>
2022-11-30 17:16:11 -0300

    feat: consumindo categorias para o formulario de criacao

--- src/components/CommerceCreateForm/NameCategoryForm.tsx
+++ src/components/CommerceCreateForm/NameCategoryForm.tsx
@@ -5,8 +5,42 @@ import {
   Input,
   Select
 } from '@chakra-ui/react';
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+interface CategoriesResponse {
+  description: string;
+  name: string;
+  __v: number;
+  _id: string;
+}
 
 export const NameCategoryForm = () => {
+  const [categories, setCategories] = useState<CategoriesResponse[]>([]);
+
+  const getCategories = async () => {
+    const token = localStorage.getItem('admin-token');
+
+    const response = await api.get('/category', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (response.status === 401) {
+      localStorage.removeItem('admin-token');
+      Router.push('/admin/login');
+      return;
+    }
+
+    setCategories(response.data.categories);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <>
       <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
@@ -26,12 +60,15 @@ export const NameCategoryForm = () => {
           id="category"
           name="category"
           autoComplete="category"
-          placeholder="Escolha uma categoria"
+          placeholder={
+            categories.length === 0 ? 'Carregando...' : 'Escolha uma categoria'
+          }
+          disabled={categories.length === 0 ? true : false}
         >
-          <option>Padarias</option>
-          <option>Mercados</option>
-          <option>Bares</option>
-          <option>Restaurantes</option>
+          {categories.length > 0 &&
+            categories.map((item, index) => (
+              <option key={index}>{item.name}</option>
+            ))}
         </Select>
       </FormControl>
     </>
