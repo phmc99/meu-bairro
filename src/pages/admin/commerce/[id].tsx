8501b36b0525094d3705cb8b98be443977bb7263
Pedro Costa <phmc99@outlook.com>
2022-12-22 12:03:56 -0300

    feat: obtendo dados da API de um comercio em sua pagina especifica

--- src/pages/admin/commerce/[id].tsx
+++ src/pages/admin/commerce/[id].tsx
@@ -1,14 +1,50 @@
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { Box, Button, Flex, Heading, Stack, Tag } from '@chakra-ui/react';
 import Router, { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import CommerceImagesSlider from '../../../components/CommerceImagesSlider';
+import api from '../../../services/api';
+import { ICommerce } from '../../../types';
 
 const CommercePage = () => {
   const router = useRouter();
+  const [commerce, setCommerce] = useState({} as ICommerce);
 
   const handleBackToDashboard = () => {
     Router.push('/admin/dashboard');
   };
+
+  const getCommerce = async () => {
+    const token = localStorage.getItem('admin-token');
+
+    if (!token) {
+      return Router.push('/admin/login');
+    }
+
+    await api
+      .get(`/commerce/${router.query.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(res => {
+        setCommerce(res.data.commerce);
+        return res.data;
+      })
+      .catch(res => {
+        if (res.response.status === 401) {
+          localStorage.removeItem('admin-token');
+          Router.push('/admin/login');
+          return;
+        }
+        return res.response.data;
+      });
+  };
+
+  useEffect(() => {
+    getCommerce();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Flex flexDirection="column" alignItems="center">
@@ -23,14 +59,17 @@ const CommercePage = () => {
       </Button>
       <Flex flexDirection="column" alignItems="center" gap={2} p={5}>
         <Heading textAlign="center" size={['md', 'xl']}>
-          Commerce {router.query.id}
+          {commerce.name}
         </Heading>
         <Stack direction={['column', 'row']}>
-          <Tag size={['sm', 'md']}>Category</Tag>
-          <Tag size={['sm', 'md']}>ID: {router.query.id}</Tag>
-          <Tag size={['sm', 'md']}>Criado em: {new Date().toUTCString()}</Tag>
-          <Tag size={['sm', 'md']} colorScheme="green">
-            Ativo
+          <Tag size={['sm', 'md']}>{commerce.category}</Tag>
+          <Tag size={['sm', 'md']}>ID: {commerce._id}</Tag>
+          <Tag size={['sm', 'md']}>Criado em: {commerce.createdAt}</Tag>
+          <Tag
+            size={['sm', 'md']}
+            colorScheme={commerce.active ? 'green' : 'red'}
+          >
+            {commerce.active ? 'Ativo' : 'Inativo'}
           </Tag>
         </Stack>
       </Flex>
