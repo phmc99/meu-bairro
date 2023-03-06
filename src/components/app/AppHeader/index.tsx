ac324aa8176ed32696ecd9108c6f0a37eaf25f23
Pedro Costa <phmc99@outlook.com>
2023-03-06 16:31:04 -0300

    fix: ajustando imagem para dispositivo ios

--- src/components/app/AppHeader/index.tsx
+++ src/components/app/AppHeader/index.tsx
@@ -3,7 +3,15 @@ import { Flex, Image } from '@chakra-ui/react';
 const AppHeader = () => {
   return (
     <Flex w="100%" h="55px" p="5px" justifyContent="center" bgColor="#f3f3f3">
-      <Image src="./header-logo.png" alt="logo" pointerEvents="none" />
+      <Image
+        maxW="150px"
+        w="100%"
+        maxH="50px"
+        h="100%"
+        src="./logo-meu-bairro.png"
+        alt="logo"
+        pointerEvents="none"
+      />
     </Flex>
   );
 };
