20064fe7b5fbdc7f07f310df226c7f30cac48948
Pedro Costa <phmc99@outlook.com>
2023-04-03 19:01:42 -0300

    feat: busca de comercios pela descricao ou nome

--- src/components/app/AppSearchInput/index.tsx
+++ src/components/app/AppSearchInput/index.tsx
@@ -1,22 +1,43 @@
-import { SearchIcon } from '@chakra-ui/icons';
 import { Flex, IconButton, Input } from '@chakra-ui/react';
+import { MdSearch, MdSearchOff } from 'react-icons/md';
 
-const AppSearchInput = () => {
+interface AppSearchInputProps {
+  value: string;
+  setValue: any;
+  handleSearch: () => void;
+  handleClearSearch: () => void;
+}
+
+const AppSearchInput = ({
+  value,
+  setValue,
+  handleSearch,
+  handleClearSearch
+}: AppSearchInputProps) => {
   return (
     <Flex w="90%" m="0 auto" p={3}>
+      <IconButton
+        aria-label="Clear search"
+        icon={<MdSearchOff size={22} />}
+        variant="link"
+        ml={2}
+        onClick={handleClearSearch}
+      />
       <Input
         placeholder="Digite sua pesquisa aqui..."
         borderRadius={0}
         borderLeftRadius={5}
-        disabled={true}
+        value={value}
+        onChange={e => setValue(e.target.value)}
       />
       <IconButton
         colorScheme="blue"
         aria-label="Search database"
-        icon={<SearchIcon />}
+        icon={<MdSearch size={22} />}
         borderRadius={0}
         borderRightRadius={5}
-        disabled={true}
+        w={20}
+        onClick={handleSearch}
       />
     </Flex>
   );
--- src/pages/app/search/index.tsx
+++ src/pages/app/search/index.tsx
@@ -3,8 +3,47 @@ import AppCategoryList from '../../../components/app/AppCategoryList';
 import AppNavBar from '../../../components/app/AppNavBar';
 import AppSearchInput from '../../../components/app/AppSearchInput';
 import NavigationHeader from '../../../components/app/NavigationHeader';
+import { useState } from 'react';
+import { ICommerce } from '../../../types';
+import api from '../../../services/api';
+import AppCommerceList from '../../../components/app/AppCommerceList';
+import AppCommerceItem from '../../../components/app/AppCommerceItem';
+import { Progress, useToast } from '@chakra-ui/react';
 
 const AppSearch = () => {
+  const [value, setValue] = useState<string>('');
+  const [page, setPage] = useState<number>(1);
+  const [hasMore, setHasMore] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [commerces, setCommerces] = useState<ICommerce[]>([]);
+
+  const toast = useToast();
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    const endpoint = `/commerce/search?value=${value}&page=${page}&perPage=10`;
+    const { data } = await api.get(endpoint);
+    setHasMore(data.next_page != null ? true : false);
+    setCommerces([...commerces, ...data.data]);
+    setPage(data.next_page);
+
+    if (data.count === 0) {
+      toast({
+        duration: 2000,
+        isClosable: true,
+        position: 'top',
+        title: 'Sem resultados para essa busca'
+      });
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleClearSearch = () => {
+    setValue('');
+    setCommerces([]);
+  };
+
   return (
     <>
       <Head>
@@ -15,8 +54,33 @@ const AppSearch = () => {
         />
       </Head>
       <NavigationHeader title="Pesquisar comércios" />
-      <AppSearchInput />
-      <AppCategoryList />
+      <AppSearchInput
+        value={value}
+        setValue={setValue}
+        handleSearch={handleSearch}
+        handleClearSearch={handleClearSearch}
+      />
+      {isLoading ? <Progress size="xs" isIndeterminate /> : null}
+      {commerces.length <= 0 ? <AppCategoryList /> : null}
+      {commerces.length > 0 ? (
+        <AppCommerceList
+          fetchMoreData={handleSearch}
+          hasMore={hasMore}
+          dataLength={commerces.length}
+        >
+          {commerces.map(item => (
+            <AppCommerceItem
+              key={item._id}
+              id={item._id}
+              category={item.category}
+              logo={item.logo}
+              name={item.name}
+              neighborhood={item.neighborhood}
+              totalRate={item.totalRate}
+            />
+          ))}
+        </AppCommerceList>
+      ) : null}
       <AppNavBar />
     </>
   );
