fe8fb1d60f2bde419159b430610e4b15b7bf1f69
Pedro Costa <phmc99@outlook.com>
2023-04-13 21:12:55 -0300

    feat: priorizando feedback do usuario logado na listagem

--- src/components/app/AppFeedbackList/index.tsx
+++ src/components/app/AppFeedbackList/index.tsx
@@ -9,12 +9,50 @@ import {
 import { IFeedback } from '../../../types';
 import AppFeedbackItem from '../AppFeedbackItem';
 import { scrolllBarCategoryList } from '../AppCategoryList/style';
+import { useEffect, useState } from 'react';
+import { decodeToken } from '../../../utils/jwt';
 
 interface AppFeedbackListProps {
   feedbacks: IFeedback[];
 }
 
 const AppFeedbackList = ({ feedbacks }: AppFeedbackListProps) => {
+  const [feedbackList, setFeedbackList] = useState<IFeedback[]>([]);
+
+  const feedbackOrder = (userId?: string) => {
+    if (userId) {
+      const userFeedback = feedbacks.find(item => item.user._id === userId);
+
+      if (!userFeedback) {
+        return setFeedbackList(feedbacks);
+      }
+
+      const feedbackIndex = feedbacks.findIndex(
+        item => item._id === userFeedback?._id
+      );
+
+      const feedbacksCopy = [...feedbacks];
+      feedbacksCopy.splice(feedbackIndex, 1);
+
+      setFeedbackList([userFeedback, ...feedbacksCopy]);
+      return;
+    }
+
+    return;
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('user-token') || '';
+
+    if (token.trim() !== '') {
+      const { decoded } = decodeToken(token);
+      const { id } = decoded as { id: string };
+      feedbackOrder(id);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <ModalOverlay />
@@ -23,8 +61,8 @@ const AppFeedbackList = ({ feedbacks }: AppFeedbackListProps) => {
         <ModalCloseButton />
         <ModalBody>
           <Box h="50vh" overflowY="scroll" sx={scrolllBarCategoryList}>
-            {feedbacks.length > 0
-              ? feedbacks.map(item => (
+            {feedbackList.length > 0
+              ? feedbackList.map(item => (
                   <AppFeedbackItem feedback={item} key={item._id} />
                 ))
               : null}
