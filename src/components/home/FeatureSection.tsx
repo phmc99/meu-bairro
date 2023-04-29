76fc0ac4c2ab378a11f1a7206a20ef32860e13c6
Pedro Costa <phmc99@outlook.com>
2023-04-29 20:19:38 -0300

    feat: secao sobre pagina home

---
+++ src/components/home/FeatureSection.tsx
@@ -0,0 +1,123 @@
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Stack,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react';
+import { ReactElement } from 'react';
+import {
+  FcAssistant,
+  FcCollaboration,
+  FcGlobe,
+  FcRating,
+  FcSearch
+} from 'react-icons/fc';
+
+interface CardProps {
+  heading: string;
+  description: string;
+  icon: ReactElement;
+}
+
+const Card = ({ heading, description, icon }: CardProps) => {
+  return (
+    <Box
+      maxW={{ base: 'full', md: '275px' }}
+      w={'full'}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={5}
+    >
+      <Stack align={'start'} spacing={2}>
+        <Flex
+          w={16}
+          h={16}
+          align={'center'}
+          justify={'center'}
+          color={'white'}
+          rounded={'full'}
+          bg={useColorModeValue('gray.100', 'gray.700')}
+        >
+          {icon}
+        </Flex>
+        <Box mt={2}>
+          <Heading size="md">{heading}</Heading>
+          <Text mt={1} fontSize={'sm'}>
+            {description}
+          </Text>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default function GridListWith() {
+  return (
+    <Flex direction="column" justifyContent="center" p={4}>
+      <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
+        <Heading fontSize={{ base: '2xl', sm: '4xl' }} fontWeight={'bold'}>
+          Sobre o Meu Bairro
+        </Heading>
+        <Text color={'gray.600'} fontSize={{ base: 'sm', sm: 'lg' }}>
+          Nossa missão é ajudar os comércios locais, que não tem uma estrutura
+          de divulgação nas redes sociais, por meio de uma plataforma que
+          facilita a interação da vizinhança com os comércios.
+        </Text>
+      </Stack>
+
+      <Container maxW={'5xl'} mt={12}>
+        <Heading
+          fontSize={{ base: '2xl', sm: '3xl' }}
+          textAlign="center"
+          color={'gray.600'}
+          fontWeight={'semibold'}
+          mb={4}
+        >
+          Beneficios do App
+        </Heading>
+        <Flex flexWrap="wrap" gridGap={6} justify="center">
+          <Card
+            heading={'Contato com o comércio'}
+            icon={<Icon as={FcCollaboration} w={10} h={10} />}
+            description={
+              'Facilitamos interação do cliente com o comércio, mantendo os dados do comércio sempre atualizados.'
+            }
+          />
+          <Card
+            heading={'Avaliação dos comércios'}
+            icon={<Icon as={FcRating} w={10} h={10} />}
+            description={
+              'As avaliações classificam o grau de satisfação que os clientes têm com os comércios. Elas são fundamentais, pois atestam a outros clientes como foi a experiência que as pessoas tiveram com o comércio.'
+            }
+          />
+          <Card
+            heading={'Busque o que precisa'}
+            icon={<Icon as={FcSearch} w={10} h={10} />}
+            description={
+              'Nossa busca é dinâmica, então você pode buscar por qualquer coisa que exista dentro do Meu Bairro'
+            }
+          />
+          <Card
+            heading={'Encontre comércios próximos'}
+            icon={<Icon as={FcGlobe} w={10} h={10} />}
+            description={
+              'Compartilhando sua localização com o Meu Bairro, podemos te mostrar os comércios mais próximos a você'
+            }
+          />
+          <Card
+            heading={'Contato rápido com o Suporte'}
+            icon={<Icon as={FcAssistant} w={10} h={10} />}
+            description={
+              'Muito útil para os comércios. Esse recurso facilita a atualização da página e mantém todos os clientes atualizados.'
+            }
+          />
+        </Flex>
+      </Container>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
 import Head from 'next/head';
-import {
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Stack,
-  Text,
-  useBreakpointValue
-} from '@chakra-ui/react';
+import { Button, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import Link from 'next/link';
+import GridListWith from '../components/home/FeatureSection';
 
 export default function Home() {
   return (
@@ -20,65 +13,52 @@ export default function Home() {
           content="Meu Bairro - App de comércios locais"
         />
       </Head>
-      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-        <Flex p={8} flex={1} align={'center'} justify={'center'}>
-          <Stack spacing={6} w={'full'} maxW={'lg'}>
-            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-              <Text
-                as={'span'}
-                position={'relative'}
-                _after={{
-                  content: "''",
-                  width: 'full',
-                  height: useBreakpointValue({ base: '20%', md: '30%' }),
-                  position: 'absolute',
-                  bottom: 1,
-                  left: 0,
-                  bg: 'blue.400',
-                  zIndex: -1
-                }}
-              >
-                Meu Bairro
+      <Flex direction="column">
+        <Stack direction={{ base: 'column', md: 'row' }}>
+          <Flex p={8} flex={1} align={'center'} justify={'center'}>
+            <Stack spacing={6} w={'full'} maxW={'lg'}>
+              <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                <Image src="./logo-meu-bairro.png" alt="logo" />
+                <Text color={'blue.400'} as={'span'}>
+                  O app dos comércios do seu bairro.
+                </Text>
+              </Heading>
+              <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+                Alavanque seu comércio de forma acessível e efetiva. Divulgando
+                seu comércio para todo bairro em nosso aplicativo, você terá
+                muito mais visibilidade e vendas.
               </Text>
-              <br />
-              <Text color={'blue.400'} as={'span'}>
-                O app dos comércios do seu bairro.
-              </Text>
-            </Heading>
-            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-              Alavanque seu comércio de forma acessível e efetiva. Divulgando
-              seu comércio para todo bairro em nosso aplicativo, você terá muito
-              mais visibilidade e vendas.
-            </Text>
-            <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-              <Button
-                rounded={'full'}
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500'
-                }}
-              >
-                Cadastrar seu comércio
-              </Button>
-              <Button rounded={'full'}>
-                <Link target="_blank" href="/app">
-                  Conheça o app
-                </Link>
-              </Button>
+              <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  rounded={'full'}
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500'
+                  }}
+                >
+                  Cadastrar seu comércio
+                </Button>
+                <Button rounded={'full'}>
+                  <Link target="_blank" href="/app">
+                    Conheça o app
+                  </Link>
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
-        </Flex>
-        <Flex flex={1}>
-          <Image
-            alt={'Login Image'}
-            objectFit={'cover'}
-            src={
-              'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            }
-          />
-        </Flex>
-      </Stack>
+          </Flex>
+          <Flex flex={1}>
+            <Image
+              alt={'Login Image'}
+              objectFit={'cover'}
+              src={
+                'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+              }
+            />
+          </Flex>
+        </Stack>
+        <GridListWith />
+      </Flex>
     </>
   );
 }
